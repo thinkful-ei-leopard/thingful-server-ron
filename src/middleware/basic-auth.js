4e80134efcd1b71ee28cd0dc13e837c24deb179e
Ron Martin <ronmartin89@gmail.com>
2020-03-05 02:59:05 -0800

    implemented our AuthService after refactoring. tests passing

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,3 +1,5 @@
+const AuthService = require('../auth/auth-service');
+
 function requireAuth(req,res,next) {
   const authToken = req.get('Authorization') || '';
   let basicToken;
@@ -7,20 +9,17 @@ function requireAuth(req,res,next) {
   } else {
     basicToken = authToken.slice('basic '.length, authToken.length);
   }
-  console.log(basicToken);
-  const [tokenUserName, tokenPassword] = Buffer
-    .from(basicToken, 'base64')
-    .toString()
-    .split(':');
+
+  const [tokenUserName, tokenPassword] = AuthService.parseBasicToken(basicToken);
 
   if (!tokenUserName || !tokenPassword) {
     return res.status(401).json({ error: 'Unauthorized request' });
   }
  
-   
-  req.app.get('db')('thingful_users')
-    .where({ user_name: tokenUserName })
-    .first()
+  AuthService.getUserWithUserName(
+    req.app.get('db'),
+    tokenUserName
+  )
     .then(user => {
       if (!user || user.password !== tokenPassword) {
         return res.status(401).json({ error: 'Unauthorized request' });
