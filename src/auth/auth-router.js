16c3fa21c75ad15c48e9151f86315c68d093e6b0
Ron Martin <ronmartin89@gmail.com>
2020-03-06 09:52:33 -0800

    Setup validation to check if a user_name or password wasnt sent in and verified all tests are passing

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,10 +1,23 @@
 const express = require('express');
 
 const authRouter = express.Router();
+const jsonParser = express.json();
 
 authRouter
-  .post('/login', (req, res, next) => {
+  .route('/login')
+  .post(jsonParser, (req, res, next) => {
+    const { user_name, password } = req.body;
+    const loginUser =  { user_name, password };
+
+    for (const [key, value] of Object.entries(loginUser)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: `Missing '${key}' in request body`
+        });
+      }
+    }
+
     res.send('ok');
   });
 
-module.export = authRouter;
+module.exports = authRouter;
