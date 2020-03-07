4b65b1cf779f756e77d9bfa9b9e0bd05e37b5178
Ron Martin <ronmartin89@gmail.com>
2020-03-06 18:13:52 -0800

    Updated our tests for protected endpoints

--- test/things-endpoints.spec.js
+++ test/things-endpoints.spec.js
@@ -54,25 +54,18 @@ describe('Things Endpoints', function() {
             .expect(401, { error: `Missing bearer token` })
         })
         it(`responds 401 'Unauthorized request' when no credentials in token`, () => {
-          const userNoCreds = { user_name: '', password: '' }
+          const validUser = testUsers[0];
+          const invalidSecret = 'bad-secret';
           return supertest(app)
             .get(endpoint.path)
-            .set('Authorization', helpers.makeAuthHeader(userNoCreds))
+            .set('Authorization', helpers.makeAuthHeader(validUser, invalidSecret))
             .expect(401, { error: `Unauthorized request` })
         })
-        it(`responds 401 'Unauthorized request' when invalid user`, () => {
-          const userInvalidCreds = { user_name: 'user-not', password: 'existy' }
+        it(`responds 401 'Unauthorized request' when invalid sub in payload`, () => {
+          const invalidUser = { user_name: 'user-not-existy', id: 1 }
           return supertest(app)
             .get(endpoint.path)
-            .set('Authorization', helpers.makeAuthHeader(userInvalidCreds))
-            .expect(401, { error: `Unauthorized request` })
-        })
-  
-        it(`responds 401 'Unauthorized request' when invalid password`, () => {
-          const userInvalidPass = { user_name: testUsers[0].user_name, password: 'wrong' }
-          return supertest(app)
-            .get(endpoint.path)
-            .set('Authorization', helpers.makeAuthHeader(userInvalidPass))
+            .set('Authorization', helpers.makeAuthHeader(invalidUser))
             .expect(401, { error: `Unauthorized request` })
         })
       })
